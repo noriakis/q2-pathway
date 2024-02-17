e9f98ff9636c28caa5708154971858d25963ce23
noriakis <nori@hgc.jp>
2024-02-17 14:02:37 +0900

    bare support for strat

--- q2_pathway/_summarize.py
+++ q2_pathway/_summarize.py
@@ -70,6 +70,8 @@ def summarize(output_dir: str,
     ## Loose checking (tax1_K00001)
     if "_" in all_cols[0]:
         strat = True
+        
+    ## As PICRUSt2 does not produce stratified table by default
     if strat:
         all_taxs = list(set([i.split("_")[0] for i in all_cols]))
         all_kos = list(set([i.split("_")[1] for i in all_cols]))
@@ -80,21 +82,49 @@ def summarize(output_dir: str,
             
             data = pd.concat([ko_table, metadata_df_filt],
                              axis=1, join='inner')
-            kos = []
-            names = []
-            groups = []
             
             for ko in all_kos:
+                prefix = column+"_"+ko
+
                 candidate_columns = [i for i in all_cols if ko in i]
-                tmp_sp = {}
-                for name, group in data.groupby(column):
-                    kos.append(ko)
-                    names.append('%s (n=%d)' % (name, len(group)))
-                    for i in candidate_columns:
-                        tmp_sp[i.split("_")[0]] = list(group[i])
-                    groups.append(tmp_sp)
-            ## We have KO list, along with group length and 
-            ## Per-KO stratified abundance per species
+                candidate_columns.append(column)
+                output = data.loc[:, candidate_columns]
+                output["sample"] = output.index.values
+                output = pd.melt(output, id_vars=["sample", column])
+                output["variable"] = output["variable"].apply(lambda x: x.split("_")[0])
+                
+                plt.figure()
+                bp = sns.boxplot(data=output, x="variable", y="value")
+                fig = bp.get_figure()
+                fig.savefig(path.join(output_dir, prefix + ".png"))
+                plt.close()
+                    
+                img = Image.open(path.join(output_dir, column+"_"+ko+".png"))
+                img_byte_arr = io.BytesIO()
+                img.save(img_byte_arr, format='PNG')
+                img_byte_arr = base64.b64encode(img_byte_arr.getvalue()).decode("utf-8")
+
+                csv_path = os.path.join(output_dir, prefix + ".csv")
+                    
+                output = output.groupby(column).apply(lambda x: x.groupby("variable").mean("value"))
+                output.to_csv(csv_path)
+
+                output["ko"] = '<a href="'+ko_url+ko+'">'+ko+'</a>'
+                
+                ## Save the image
+                jsonp = prefix + ".jsonp"
+                with open(os.path.join(output_dir, jsonp), 'w') as fh:
+                    fh.write('load_data("%s",' % prefix)
+                    json.dump(img_byte_arr, fh)
+                    fh.write(",'")
+                    table = q2templates.df_to_html(output, escape=False)
+                    fh.write(table.replace('\n', '').replace("'", "\\'"))
+                    fh.write("','")
+                    fh.write(quote(prefix+".csv"))
+                    fh.write("');")
+                filenames.append(jsonp)
+                ## Per-KO stratified abundance per species
+                
     else:
         all_kos = list(set(all_cols))
         for column in metadata_df.columns:
