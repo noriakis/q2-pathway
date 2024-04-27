d6541536f72e9232235f6a8ab52092f69e016717
noriakis <nori@hgc.jp>
2024-04-27 12:13:59 +0900

    setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 from setuptools import setup, find_packages
 
 setup(
@@ -8,14 +7,18 @@ setup(
     author="Noriaki Sato",
     author_email="nori@hgc.jp",
     description="QIIME2 plugin for analyzing biological pathway information based on gene family abundances.",
-    license='BSD-3-Clause',
+    license="BSD-3-Clause",
     url="https://github.com/noriakis/q2-pathway",
-    entry_points={
-        'qiime2.plugins': ['q2-pathway=q2_pathway.plugin_setup:plugin']
-    },
+    entry_points={"qiime2.plugins": ["q2-pathway=q2_pathway.plugin_setup:plugin"]},
     package_data={
-        'q2_pathway': ['citations.bib', 'assets/index.html', 'assets/dist/*', 'assets/*.R',
-        'assets/*.fasta.gz', 'assets/*.tsv.gz']
+        "q2_pathway": [
+            "citations.bib",
+            "assets/index.html",
+            "assets/dist/*",
+            "assets/*.R",
+            "assets/*.fasta.gz",
+            "assets/*.tsv.gz",
+        ]
     },
     zip_safe=False,
 )
