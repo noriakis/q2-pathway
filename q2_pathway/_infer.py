73a9c9de8e81eeeac394a2980d835cf5df13185f
noriakis <31095487+noriakis@users.noreply.github.com>
2024-06-25 12:36:49 +0900

    piphillin fix

--- q2_pathway/_infer.py
+++ q2_pathway/_infer.py
@@ -30,89 +30,52 @@ def infer(
             for seqname, sequence in sequences.items():
                 print(">" + str(seqname) + "\n" + str(sequence), file=fna)
         seq_table.T.to_csv(path.join(temp_dir, "seqtab.txt"), sep="\t")
-        if algorithm == "piphillin":
-            cmd = [
-                "vsearch",
-                "--usearch_global",
-                repseq,
-                "--db",
-                reference_sequences,
-                "--id",
-                str(pct_id),
-                "--top_hits_only",
-                "--maxaccepts",
-                "0",
-                "--maxrejects",
-                "0",
-                "--uc_allhits",
-                "--blast6out",
-                path.join(temp_dir, "blast_out.txt"),
-                "--threads",
-                str(threads),
-            ]
-            try:
-                subprocess.run(cmd, check=True)
-            except subprocess.CalledProcessError as _:
-                raise ValueError("Error running vsearch.")
+        cmd = [
+            "vsearch",
+            "--usearch_global",
+            repseq,
+            "--db",
+            reference_sequences,
+            "--id",
+            str(pct_id),
+            "--top_hits_only",
+            "--maxaccepts",
+            "0",
+            "--maxrejects",
+            "0",
+            "--uc_allhits",
+            "--blast6out",
+            path.join(temp_dir, "blast_out.txt"),
+            "--threads",
+            str(threads),
+        ]
+        try:
+            subprocess.run(cmd, check=True)
+        except subprocess.CalledProcessError as _:
+            raise ValueError("Error running vsearch.")
 
-            cmd = [
-                "Rscript",
-                path.join(TEMPLATES, "perform_piphillin.R"),
-                temp_dir,
-                cn_table,
-                cn_16s_table,
-                str(full),
-            ]
-            try:
-                subprocess.run(cmd, check=True)
-            except subprocess.CalledProcessError as _:
-                raise ValueError("Error running piphillin algorithm.")
-            ko = pd.read_csv(path.join(temp_dir, "ko_table.tsv"), sep="\t", header=0)
+        cmd = [
+            "Rscript",
+            path.join(TEMPLATES, "perform_piphillin.R"),
+            temp_dir,
+            cn_table,
+            cn_16s_table,
+            str(full),
+        ]
+        try:
+            subprocess.run(cmd, check=True)
+        except subprocess.CalledProcessError as _:
+            raise ValueError("Error running piphillin algorithm.")
+        ko = pd.read_csv(path.join(temp_dir, "ko_table.tsv"), sep="\t", header=0)
 
-            if full:
-                ko.index = ko.iloc[:, 0].map(str) + "_" + ko.iloc[:, 1]
-                ko = ko.drop(ko.columns[0], axis=1)
-                ko = ko.drop(ko.columns[0], axis=1)
-                ## This will output tax1_K00001 type index
+        if full:
+            ko.index = ko.iloc[:, 0].map(str) + "_" + ko.iloc[:, 1]
+            ko = ko.drop(ko.columns[0], axis=1)
+            ko = ko.drop(ko.columns[0], axis=1)
+            ## This will output tax1_K00001 type index
 
-            return ko.T
-        elif algorithm == "tax4fun2":
-            ## Although the input filepath is discouraged, the Tax4Fun2 database is structured
-            ## inside the archive and cannot be properly converted to QZA.
-            if reference_database is None:
-                raise ValueError(
-                    "Please provide Tax4Fun2 default database artifact to `reference_database`."
-                )
-                
-            db = qiime2.Artifact.load(reference_database)
-            d = db.view(T4F2DirectoryFormat)
-            db_path = d.path
-            
-            cmd = [
-                "Rscript",
-                path.join(TEMPLATES, "perform_tax4fun2.R"),
-                temp_dir,
-                "rep_seqs.fna",  ## rep-seqs
-                "seqtab.txt",
-                reference_database,
-                str(pct_id),
-                str(threads),
-            ]
-            try:
-                subprocess.run(cmd, check=True)
-            except subprocess.CalledProcessError as _:
-                raise ValueError("Error running tax4fun2 algorithm.")
-            ko = pd.read_csv(
-                path.join(temp_dir, "functional_prediction.txt"),
-                sep="\t",
-                header=0,
-                index_col=0,
-            )
-            ko = ko.drop("description", axis=1)
-            return ko.T
+        return ko.T
 
-        else:
-            raise ValueError("Please specify appropriate algorithm name")
 
 
 def infer_t4f2(
